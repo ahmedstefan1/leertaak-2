fbecc7f503ec1a2faafb891a0b6fafb815403e9d
ahmedstefan1 <34337955+ahmedstefan1@users.noreply.github.com>
2019-03-24 12:24:55 +0100

    had ze eerst in de foute folder geupload

--- leertaak 2/Login.php
+++ leertaak 2/Login.php
@@ -11,7 +11,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-    <title>Weatherstation Coolerts</title>
+    <title>Onzebank</title>
 </head>
 
 <body id="login">
@@ -60,7 +60,6 @@
                   <div class="card card-signin my-5">
                       <div class="card-body">
                           <div class="logo-container">
-                              <img src="img/logo.jpg" alt="Logo University">
                           </div>
                           <form class="form-signin" method="post" action ="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
